b236ced42e36c41360b2bee31e478f9154c9d54c
Benny William Pardede <bpardedewilliam@gmail.com>
2019-10-19 15:45:03 +0700

    latihan hof and lc

--- merge.hs
+++ merge.hs
@@ -1,7 +1,8 @@
 merge xxs@(x:xs) yys@(y:ys) | x == y = x : merge xs ys 
                             | x < y  = x : merge xs yys 
-                            | x > y    = y : merge xxs ys
+                            | x > y  = y : merge xxs ys
 
 mergeSort [] = []
 mergeSort [x] = [x]
-mergeSort xs = merge (mergeSort (take (floor(length xs) / 2) xs)) (mergeSort (drop (floor (length xs) / 2) xs))
+mergeSort xs = merge (mergeSort (take (floor (n `div` 2)) xs)) (mergeSort (drop (floor (n `div` 2)) xs))
+				where n = length xs
